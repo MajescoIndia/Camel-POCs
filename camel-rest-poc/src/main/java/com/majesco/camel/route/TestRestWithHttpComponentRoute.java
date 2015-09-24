be310f557b91062e49ff201b4980ebcae7aa3240
unknown <biplab661592@IND-MHP3DW79532.mastek.com>
2015-09-24 14:21:54 +0530

    fixed POST request body issue

--- camel-rest-poc/src/main/java/com/majesco/camel/route/TestRestWithHttpComponentRoute.java
+++ camel-rest-poc/src/main/java/com/majesco/camel/route/TestRestWithHttpComponentRoute.java
@@ -3,6 +3,7 @@ package com.majesco.camel.route;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.majesco.xsd.schema.rest.sample.UserDetails;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -20,13 +21,12 @@ public class TestRestWithHttpComponentRoute extends RouteBuilder {
         //jacksonDataFormat.setUnmarshalType(UserDetails.class);
 
         from("timer:callRestService?delay=10000")
-                .log(LoggingLevel.INFO, "${headers}")
-                //.setHeader(Exchange.HTTP_METHOD, simple("POST"))
+                .streamCaching()
                 .to("http://localhost:8080/rest-server/cxf/rest/user/get")
+                .log(LoggingLevel.INFO, "${headers}")
                 .log(LoggingLevel.INFO, "${body}")
                 .setHeader(Exchange.HTTP_METHOD, simple("POST"))
                 .setHeader(Exchange.CONTENT_TYPE, simple("application/json"))
-                .to("http://localhost:8080/rest-server/cxf/rest/user/save")
-                .log(LoggingLevel.INFO, "++++++++++++++ ${body}");
+                .to("http://localhost:8080/rest-server/cxf/rest/user/save");
     }
 }
